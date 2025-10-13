de318fa880db0febb2c79440d89d8579c52007ca
Khaleb Denham <khalebdenham@gmail.com>
2025-10-13 14:27:49 -0400

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import Container from "react-bootstrap/Container";
+import Dropdown from "react-bootstrap/Dropdown";
+import { Nav } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
+import NavItem from "react-bootstrap/NavItem";
+import { Navbar } from "react-bootstrap";
+import NavLink from "react-bootstrap/NavLink";
+
+function ColorSchemesExample() { //This is a modified navbar in which I utilized the nav items and dropdown options.
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark"> 
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Dropdown as={NavItem}>
+              <Dropdown.Toggle as={NavLink}>Click to see more…</Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item>Hello there!</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+export default ColorSchemesExample;
